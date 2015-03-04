821dfd2bea69df9ed6201ba3ae5fe19a4a92fb4f
David Vesely <david.vesely@erni.sk>
2015-03-04 17:48:57 +0100

    stats report for last 10 mates

--- src/main/java/ch/erni/community/footsign/repository/MatchRepository.java
+++ src/main/java/ch/erni/community/footsign/repository/MatchRepository.java
@@ -1,5 +1,6 @@
 package ch.erni.community.footsign.repository;
 
+import ch.erni.community.footsign.nodes.Game;
 import ch.erni.community.footsign.nodes.Match;
 import ch.erni.community.footsign.nodes.User;
 import org.springframework.data.neo4j.annotation.Query;
@@ -17,5 +18,15 @@ public interface MatchRepository extends CrudRepository<Match, Long> {
 
 	@Query("MATCH (n)-[r:TEAM1]->(m) RETURN m")
 	List<User> findAllPlayedPlayers();
+	@Query("match(user:User {domainShortName: {0}})--(m:Match)-[r:GAMES]->(g) return g")
+	List<Game> findAllGamesByUserDomainShortName(String domainShortName);
 
+	@Query("match(user:User {domainShortName: {0}})--(m:Match)-[r:GAMES]->(g) WHERE g.team1Result = 8 return g")
+	List<Game>findAllTeam1WinsGameByUserDomainShortName(String doma);
+
+	@Query("match(user:User {domainShortName: {0}})--(m:Match)-[r:GAMES]->(g) WHERE g.team2Result = 8 return g")
+	List<Game>findAllTeam2WinsGameByUserDomainShortName(String doma);
+
+	@Query("MATCH (n)-[r:TEAM1]->(m) RETURN DISTINCT n ORDER BY n.dateOfMatch DESC LIMIT 10")
+	List<Match> findlastMatches();
 }
