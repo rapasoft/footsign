0378d34b4241db4ebbb25bb03535754b5fe53c50
Pavol Rajzák <pavol.rajzak@erni.sk>
2015-03-05 16:34:07 +0100

    Created statistics for best player/most played games

--- src/main/java/ch/erni/community/footsign/ServletApplication.java
+++ src/main/java/ch/erni/community/footsign/ServletApplication.java
@@ -1,15 +1,10 @@
 package ch.erni.community.footsign;
 
-import ch.erni.community.footsign.configuration.DataConfiguration;
-import org.neo4j.kernel.impl.util.FileUtils;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import java.io.File;
-import java.io.IOException;
 
 /**
  * @author rap
@@ -23,11 +18,11 @@ public class ServletApplication extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		try {
-			FileUtils.deleteRecursively(new File(DataConfiguration.NEO4J_DB_PATH));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+//		try {
+//			FileUtils.deleteRecursively(new File(DataConfiguration.NEO4J_DB_PATH));
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 		return application.sources(applicationClass);
 	}
 
--- src/main/java/ch/erni/community/footsign/repository/MatchRepository.java
+++ src/main/java/ch/erni/community/footsign/repository/MatchRepository.java
@@ -16,7 +16,7 @@ public interface MatchRepository extends CrudRepository<Match, Long> {
 	@Query("match(user:User {domainShortName: {0}})--(m:Match) return m")
 	List<Match> findAllByUserDomainShortName(String domainShortName);
 
-	@Query("MATCH (user:User)--(m: Match) RETURN user")
+	@Query("MATCH (n)-[r:TEAM1]->(m) RETURN m")
 	List<User> findAllPlayedPlayers();
 
 	@Query("match(user:User {domainShortName: {0}})--(m:Match)-[r:GAMES]->(g) return g")
@@ -30,4 +30,30 @@ public interface MatchRepository extends CrudRepository<Match, Long> {
 
 	@Query("MATCH (n)-[r:TEAM1]->(m) RETURN DISTINCT n ORDER BY n.dateOfMatch DESC LIMIT 10")
 	List<Match> findlastMatches();
+
+	@Query("match (u:User)--(m:Match) with u,count(m) as matches return u order by matches desc limit 1")
+	User findPlayerWithMostPlayedMatches();
+
+	// TODO @rap: make generic for user
+	@Query("match (u:User)--(m:Match) with u, count(distinct(m)) as matches return matches order by matches desc limit 1")
+	int countPlayedMatches(User userWmostPlayed);
+
+	@Query("match (u:User)<--(m:Match)-->(g:Game) \n" +
+			"where (((u)-[:TEAM1]-(m)-->(g) and g.team1Result = 8) OR ((u)-[:TEAM2]-(m)-->(g) and g.team2Result = 8))\n" +
+			"with u,m,count(g) as countGames \n" +
+			"where countGames >= 2\n" +
+			"with u,count(distinct m) as matches\n" +
+			"return u " +
+			"order by matches desc limit 1")
+	User findPlayerWithMostWins();
+
+	// TODO @rap: make generic for user
+	@Query("match (u:User)<--(m:Match)-->(g:Game) \n" +
+			"where (((u)-[:TEAM1]-(m)-->(g) and g.team1Result = 8) OR ((u)-[:TEAM2]-(m)-->(g) and g.team2Result = 8))\n" +
+			"with u,m,count(g) as countGames \n" +
+			"where countGames >= 2\n" +
+			"with u,count(distinct m) as matches\n" +
+			"return matches " +
+			"order by matches desc limit 1")
+	int countWonMatches(User userWmostWins);
 }
