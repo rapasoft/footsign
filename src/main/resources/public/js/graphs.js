bc4fb0be095c3fc82433e4ebaa51f34497364412
Peter Cervienka <peter.cervienka@erni.sk>
2015-03-09 12:30:21 +0100

    refactor graph module

---
+++ src/main/resources/public/js/graphs.js
@@ -0,0 +1,22 @@
+/**
+ * Created by cepe on 09.03.2015.
+ */
+
+function getDataForPieGraph(url, title, componentId) {
+
+    $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'json',
+        success:function(data) {
+            var data = google.visualization.arrayToDataTable(data);
+            var options = {
+                title: title
+            };
+            var chart = new google.visualization.PieChart(document.getElementById(componentId));
+
+            chart.draw(data, options);
+        }
+
+    });
+}
