6dd1ebbd0bff0cef78de9b80d2b5409ad15f577e
Peter Cervienka <peter.cervienka@erni.sk>
2015-03-12 16:45:55 +0100

    cache resources

--- src/main/java/ch/erni/community/footsign/configuration/WebMvcConfiguration.java
+++ src/main/java/ch/erni/community/footsign/configuration/WebMvcConfiguration.java
@@ -1,16 +1,26 @@
 package ch.erni.community.footsign.configuration;
 
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.resource.AppCacheManifestTransformer;
+import org.springframework.web.servlet.resource.GzipResourceResolver;
 
 /**
  * @author rap
  */
 @Configuration
-public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
+@AutoConfigureAfter(DispatcherServletAutoConfiguration.class)
+public class WebMvcConfiguration extends WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter {
 
+	private final int CACHE_PERIOD = 86400;
+	
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/rules").setViewName("rules");
@@ -18,4 +28,18 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 		registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
 	}
 
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		super.addResourceHandlers(registry);
+
+		// TODO: not working :(
+		registry
+				.addResourceHandler("/resources/**")
+				.addResourceLocations("/resources/", "classpath:/resources/")
+				.setCachePeriod(CACHE_PERIOD)
+				.resourceChain(true)
+				.addResolver(new GzipResourceResolver())
+				.addTransformer(new AppCacheManifestTransformer());
+		
+	}
 }
