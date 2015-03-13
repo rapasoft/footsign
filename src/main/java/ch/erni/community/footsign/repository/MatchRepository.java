0dc4bb19c117004aae0bac5d637f3a02302d9950
David Vesely <david.vesely@erni.sk>
2015-03-13 10:23:51 +0100

    Change selects for plan matches.

--- src/main/java/ch/erni/community/footsign/repository/MatchRepository.java
+++ src/main/java/ch/erni/community/footsign/repository/MatchRepository.java
@@ -39,7 +39,7 @@ public interface MatchRepository extends CrudRepository<Match, Long>, MatchRepos
 			"order by value desc limit 10")
 	List<CustomPlayer> findTenPlayersWithHighestRatio();
 	
-	@Query("match (u:User)<--(m:Match)-->(g:Game) \n" +
+	@Query("match (u:User)<--(m:Match {planed : false})-->(g:Game) \n" +
 			"where (((u)-[:TEAM1]-(m)-->(g) and g.team1Result = 8) OR ((u)-[:TEAM2]-(m)-->(g) and g.team2Result = 8))\n" +
 			"with u,m,count(g) as countGames \n" +
 			"where countGames >= 2\n" +
