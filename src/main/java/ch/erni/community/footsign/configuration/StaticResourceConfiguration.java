27257345018b054eb4b34960108396abca82af9a
Pavol Rajzák <pavol.rajzak@erni.sk>
2015-03-18 13:04:32 +0100

    Enabled GZIP compression for production profile

--- src/main/java/ch/erni/community/footsign/configuration/StaticResourceConfiguration.java
+++ src/main/java/ch/erni/community/footsign/configuration/StaticResourceConfiguration.java
@@ -3,12 +3,16 @@ package ch.erni.community.footsign.configuration;
 import ch.erni.community.footsign.exception.PropertyFileNotFound;
 import ch.erni.community.footsign.util.PhotoPathBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.resource.AppCacheManifestTransformer;
 import org.springframework.web.servlet.resource.GzipResourceResolver;
+import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
 import org.springframework.web.servlet.resource.VersionResourceResolver;
 
 /**
@@ -17,7 +21,8 @@ import org.springframework.web.servlet.resource.VersionResourceResolver;
  * Time: 14:30
  */
 @Configuration
-public class StaticResourceConfiguration extends WebMvcConfigurerAdapter {
+@AutoConfigureAfter(DispatcherServletAutoConfiguration.class)
+public class StaticResourceConfiguration extends WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter {
 
 	@Autowired
 	private Environment env;
@@ -31,12 +36,16 @@ public class StaticResourceConfiguration extends WebMvcConfigurerAdapter {
 		addGzippedStaticResourceHandler(registry);
 	}
 
+	@Bean
+	public ResourceUrlEncodingFilter resourceUrlEncodingFilter() {
+		return new ResourceUrlEncodingFilter();
+	}
+
 	private void addGzippedStaticResourceHandler(ResourceHandlerRegistry registry) {
 		boolean isCacheEnabled = Boolean.valueOf(env.getProperty("spring.thymeleaf.cache"));
 
-		registry.addResourceHandler("/public/css/**", "/public/js/**")
-				.addResourceLocations("/public/css/", "classpath:/public/css")
-				.addResourceLocations("/public/js", "classpath:/public/js")
+		registry.addResourceHandler("/**")
+				.addResourceLocations("/public/", "classpath:/public/")
 				.setCachePeriod(Integer.parseInt(env.getProperty("spring.resources.cache-period")))
 				.resourceChain(isCacheEnabled)
 				.addResolver(new GzipResourceResolver())
