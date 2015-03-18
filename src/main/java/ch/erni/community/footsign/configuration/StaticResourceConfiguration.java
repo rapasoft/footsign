b83cbe1502edd14436adecb487d147c184be5536
Pavol Rajzák <pavol.rajzak@erni.sk>
2015-03-18 10:18:00 +0100

    Added minification of css and js files and enabled cache.

--- src/main/java/ch/erni/community/footsign/configuration/StaticResourceConfiguration.java
+++ src/main/java/ch/erni/community/footsign/configuration/StaticResourceConfiguration.java
@@ -4,8 +4,12 @@ import ch.erni.community.footsign.exception.PropertyFileNotFound;
 import ch.erni.community.footsign.util.PhotoPathBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.resource.AppCacheManifestTransformer;
+import org.springframework.web.servlet.resource.GzipResourceResolver;
+import org.springframework.web.servlet.resource.VersionResourceResolver;
 
 /**
  * User: ban
@@ -16,10 +20,31 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class StaticResourceConfiguration extends WebMvcConfigurerAdapter {
 
 	@Autowired
+	private Environment env;
+
+	@Autowired
 	private PhotoPathBuilder photoPathBuilder;
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		addAvatarsResourceHandler(registry);
+		addGzippedStaticResourceHandler(registry);
+	}
+
+	private void addGzippedStaticResourceHandler(ResourceHandlerRegistry registry) {
+		boolean isCacheEnabled = Boolean.valueOf(env.getProperty("spring.thymeleaf.cache"));
+
+		registry.addResourceHandler("/public/css/**", "/public/js/**")
+				.addResourceLocations("/public/css/", "classpath:/public/css")
+				.addResourceLocations("/public/js", "classpath:/public/js")
+				.setCachePeriod(Integer.parseInt(env.getProperty("spring.resources.cache-period")))
+				.resourceChain(isCacheEnabled)
+				.addResolver(new GzipResourceResolver())
+				.addResolver(new VersionResourceResolver().addContentVersionStrategy("/**"))
+				.addTransformer(new AppCacheManifestTransformer());
+	}
+
+	private void addAvatarsResourceHandler(ResourceHandlerRegistry registry) {
 		try {
 			registry.addResourceHandler("/avatars/**")
 					.addResourceLocations("file:" + photoPathBuilder.buildAvatarsPath());
