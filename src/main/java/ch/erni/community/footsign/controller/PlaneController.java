f6e4c86d49f82dbfb3826fa4fb1d8c0dc626c103
Peter Cervienka <peter.cervienka@erni.sk>
2015-03-19 15:15:45 +0100

    Add ability to plan match with a specific users #14

--- src/main/java/ch/erni/community/footsign/controller/PlaneController.java
+++ src/main/java/ch/erni/community/footsign/controller/PlaneController.java
@@ -54,46 +54,50 @@ public class PlaneController {
     
     private Map<Long, PlannedMatch> createPlannedMap(Calendar day) {
         Map<Long, PlannedMatch> result = new TreeMap<>();
-        Calendar cal = Calendar.getInstance();
+        Calendar startCal = Calendar.getInstance();
         Calendar endCal = Calendar.getInstance();
-        Calendar currentDate = Calendar.getInstance();
         
-        cal.setTime(day.getTime());
-        cal.set(Calendar.HOUR_OF_DAY, START_HOUR);
-        if (currentDate.get(Calendar.DAY_OF_MONTH) == day.get(Calendar.DAY_OF_MONTH) && currentDate.get(Calendar.MONTH) == day.get(Calendar.MONTH)) {
-            int hoursValue = currentDate.get(Calendar.HOUR_OF_DAY);
-            int minutesValue = currentDate.get(Calendar.MINUTE);
-            
-            if (hoursValue < END_HOUR) {
-                int min = minutesValue % INTERVAL;
-                int minutes = minutesValue - min + INTERVAL;
-                
-                cal.set(Calendar.MINUTE, minutes == 60 ? 0 : minutes);
-                cal.set(Calendar.HOUR_OF_DAY, minutes == 60 ? hoursValue + 1 : hoursValue);
-            } else {
-                cal.set(Calendar.HOUR_OF_DAY, END_HOUR);
-            }
-        }
+        startCal.setTime(day.getTime());
+        startCal.set(Calendar.HOUR_OF_DAY, START_HOUR);
+        setStartTime(day, startCal);
 
         endCal.setTime(day.getTime());
         endCal.set(Calendar.HOUR_OF_DAY, END_HOUR);
         
-        List<Match> plannedMatches = matchRepository.findAllPlanMatchesForToday(cal.getTimeInMillis(), endCal.getTimeInMillis());
+        List<Match> plannedMatches = matchRepository.findAllPlanMatchesForToday(startCal.getTimeInMillis(), endCal.getTimeInMillis());
         
-        while (cal.getTimeInMillis() <= endCal.getTimeInMillis()) {
-            PlannedMatch pl = new PlannedMatch(new Date(cal.getTimeInMillis()));
+        while (startCal.getTimeInMillis() <= endCal.getTimeInMillis()) {
+            PlannedMatch pl = new PlannedMatch(new Date(startCal.getTimeInMillis()));
             if (plannedMatches != null && !plannedMatches.isEmpty()) {
-                Optional<Match> match = plannedMatches.stream().filter(m -> m.getDateOfMatch() == cal.getTimeInMillis()).findFirst();
+                Optional<Match> match = plannedMatches.stream().filter(m -> m.getDateOfMatch() == startCal.getTimeInMillis()).findFirst();
                 pl.setTimestamp(match.isPresent() ? match.get().getDateOfMatch() : null);
             }
 
-            result.put(cal.getTimeInMillis(), pl);
+            result.put(startCal.getTimeInMillis(), pl);
 
-            cal.add(Calendar.MINUTE, INTERVAL);
+            startCal.add(Calendar.MINUTE, INTERVAL);
         }
         
         
         return result;
+    }
+
+    private void setStartTime(Calendar initialDate, Calendar startDate) {
+        Calendar currentDate = Calendar.getInstance();
+        if (currentDate.get(Calendar.DAY_OF_MONTH) == initialDate.get(Calendar.DAY_OF_MONTH) && currentDate.get(Calendar.MONTH) == initialDate.get(Calendar.MONTH)) {
+            int hoursValue = currentDate.get(Calendar.HOUR_OF_DAY);
+            int minutesValue = currentDate.get(Calendar.MINUTE);
+
+            if (hoursValue < END_HOUR) {
+                int min = minutesValue % INTERVAL;
+                int minutes = minutesValue - min + INTERVAL;
+
+                startDate.set(Calendar.MINUTE, minutes == 60 ? 0 : minutes);
+                startDate.set(Calendar.HOUR_OF_DAY, minutes == 60 ? hoursValue + 1 : hoursValue);
+            } else {
+                startDate.set(Calendar.HOUR_OF_DAY, END_HOUR);
+            }
+        }
     }
 
     @RequestMapping(value = "/saveMatchAsPlanned", method = RequestMethod.POST)
@@ -170,14 +174,14 @@ public class PlaneController {
         if (plannedMatch.getTimestamp() == null)
             throw new IllegalArgumentException("Time of planned match is not specified");
 
-        if (plannedMatch.getTeam1() == null)
-            throw new IllegalArgumentException("Team 1 is not specified");
-
-        if (plannedMatch.getTeam2() == null)
-            throw new IllegalArgumentException("Team 2 is not specified");
-        
         List<String> team1 = plannedMatch.getTeam1();
         List<String> team2 = plannedMatch.getTeam2();
+
+        /*if (team1 == null)
+            throw new IllegalArgumentException("Team 1 is not specified");
+
+        if (team2 == null)
+            throw new IllegalArgumentException("Team 2 is not specified");
         long count1 = team1.stream().filter(p -> !p.isEmpty()).count();
         long count2 = team2.stream().filter(p -> !p.isEmpty()).count();
 
@@ -189,18 +193,18 @@ public class PlaneController {
 
 
         if (count2 < 1 || count2 > 2)
-            throw new IllegalArgumentException("Number of players in team 2 have to be 1 or 2! Current number is " + count2);
+            throw new IllegalArgumentException("Number of players in team 2 have to be 1 or 2! Current number is " + count2);*/
 
         String users = "";
         for (String player : team1) {
-            if (users.contains(player))
+            if (!player.isEmpty() && users.contains(player))
                 throw new IllegalArgumentException("User can not play more than once: " + player);
             
             users += (player + ";");
         }
 
         for (String player : team2) {
-            if (users.contains(player))
+            if (!player.isEmpty() && users.contains(player))
                 throw new IllegalArgumentException("User can not play more than once: " + player);
 
             users += (player + ";");
@@ -214,16 +218,16 @@ public class PlaneController {
             match.setPlaned(true);
             match.setDateOfMatch(plannedMatch.getTimestamp());
             
-            plannedMatch.getTeam1().forEach(p -> {
+            plannedMatch.getTeam1().stream().filter(p -> !p.isEmpty()).forEach(p -> {
                 User u = userRepository.findByDomainShortName(p);
                 match.addPlayersToTeam1(u);
             });
 
-            plannedMatch.getTeam2().forEach(p -> {
+            plannedMatch.getTeam2().stream().filter(p -> !p.isEmpty()).forEach(p -> {
                 User u = userRepository.findByDomainShortName(p);
                 match.addPlayersToTeam2(u);
             });
-            
+
             return match;
         }
         return null;
