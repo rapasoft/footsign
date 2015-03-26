2aacb111c60113705f6b1c339f469f76a6d9a160
Peter Cervienka <peter.cervienka@erni.sk>
2015-03-26 11:16:16 +0100

    fixing the problem with caching static files

--- src/main/java/ch/erni/community/footsign/component/UserHolder.java
+++ src/main/java/ch/erni/community/footsign/component/UserHolder.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import sun.plugin.liveconnect.SecurityContextHelper;
 
 /**
  * Created by cepe on 25.03.2015.
